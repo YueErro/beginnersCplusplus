a5f8dfca5e0c3ff32fdeb834970ba43633a4191e
yueerro <yue.trbj@gmail.com>
2019-06-06 20:16:36 +0200

    add arithmetic

---
+++ arithmetic/src/arithmetic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  double value1 = (double)7/2;
+  int value2 = (int)7/3;
+  int value3 = 8;
+  int value4 = 10;
+  int value5 = 13%5;
+  double equiation = ( (5/4)%2 ) + (2.3*6);
+
+  value3 += 1;
+  value4 /= 5;
+
+  cout << value1 << endl;
+  cout << value2 << endl;
+  cout << value3 << endl;
+  cout << value4 << endl;
+  cout << value5 << endl;
+  cout << equiation << endl;
+
+  return 0;
+}
