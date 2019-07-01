7280b08888c827f6f529b892c801fb2384a12592
yueerro <yue.trbj@gmail.com>
2019-07-01 11:07:25 +0200

    add loop pkgs

---
+++ while/src/while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int i = 0;
+
+  while(i <=5){
+    cout << "Hello " << i << endl;
+    i += 1;
+  }
+
+  cout << "Programming quitting." << endl;
+
+  return 0;
+}
